9aa7ecaaee6fd0145b490e7b6b9c8dbf8e5c1cf0
Barkley Santo <barkley.santo@gmail.com>
2020-11-22 18:22:49 -0600

    updated form

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  def create
+    # find the restaurant thanks to the nesting because the root will
+    # need to have a restau id in the url,so we can find rest that we are reviewing
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # create the review using the param content which is gonna come from the form
+    @review = Review.new(review_params)
+    # link the review to the restaurant
+    @review.restaurant = @restaurant
+    # if it saved render that restaurants show page
+    # if not, see the show page again
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
